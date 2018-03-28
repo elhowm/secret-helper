16dc47a1baf8b41ca973939f4dc4f03244df2c11
Kris Aberfort <elhowm@gmail.com>
2018-03-28 22:04:10 +0300

    Reload chrome once per day; Exit after 10 fails

--- app.rb
+++ app.rb
@@ -12,12 +12,16 @@ require_relative 'lib/frequency_controller'
 require_relative 'lib/notifier'
 require_relative 'lib/watcher'
 
+errors_count = 0
 loop do
   begin
     Watcher.instance.perform
+    errors_count = 0
     sleep(180) # every 3 minutes
   rescue => exception
     Notifier.instance.notify_error! exception
-    sleep(20)
+    errors_count += 1
+    exit(0) if errors_count >= 10
+    sleep(30)
   end
 end
--- lib/watcher.rb
+++ lib/watcher.rb
@@ -1,29 +1,47 @@
 class Watcher
   include Singleton
 
-  attr_reader :driver, :logger
+  attr_reader :driver, :logger, :capabilities, :operations_count
 
   LOGFILE_SIZE = 1024 * 1000 # 1 megabyte
   LOGFILES_COUNT = 7
 
   def initialize
-    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    @capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
       chromeOptions: { args: %w[headless disable-gpu no-sandbox] }
     )
-    @driver = Selenium::WebDriver.for :chrome, desired_capabilities: capabilities
     @logger = Logger.new('status.log', LOGFILES_COUNT, LOGFILE_SIZE)
+    @operations_count = 0
+    @driver = nil
+    load_new_driver!
   end
 
   def perform
+    load_new_driver! if operations_count > 480 # about once per day..
     Settings.instance.refresh
     checker = StatusChecker.new(driver, logger)
 
-    return unless checker.critical?
+    return inc_operations! unless checker.critical?
 
     controller = FrequencyController.new(driver, logger)
     controller.put_down!
 
     Notifier.instance.notify!(checker.max_temp, controller.new_frequency)
     logger.info "Info letter sent. Relaxing."
+    inc_operations!
+  end
+
+  private
+
+  def inc_operations!
+    @operations_count += 1
+  end
+
+  def load_new_driver!
+    @driver.quit unless @driver.nil?
+    @driver = nil
+    @driver = Selenium::WebDriver.for :chrome, desired_capabilities: @capabilities
+    @operations_count = 0
+    logger.info "New driver loaded."
   end
 end
